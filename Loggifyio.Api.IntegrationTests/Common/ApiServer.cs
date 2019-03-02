59917e4a0e2c466f38271064cf5f368cea0cbc69
axelspangberg <axspangberg@gmail.com>
2019-03-02 15:58:28 +0100

    Start adding integrationtests

---
+++ Loggifyio.Api.IntegrationTests/Common/ApiServer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using loggifyio;
+using Loggifyio.Api.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace Loggifyio.Api.IntegrationTests.Common
+{
+    public class ApiServer : IDisposable
+    {
+        public const string Username = "admin";
+        public const string Password = "admin";
+        public TestServer Server { get; set; }
+        public HttpClient Client { get; set; }
+
+        private IConfigurationRoot _config;
+
+        public ApiServer()
+        {
+            _config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = GetAuthenticatedClient(Username, Password);
+
+        }
+
+        private HttpClient GetAuthenticatedClient(string username, string password)
+        {
+            var client = Server.CreateClient();
+            var response = client.PostAsync("/api/Login/Authenticate",
+                new JsonContent(new LoginModel { Password = password, Username = username })).Result;
+
+            response.EnsureSuccessStatusCode();
+
+            var data = JsonConvert.DeserializeObject<UserWithTokenModel>(response.Content.ReadAsStringAsync().Result);
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + data.Token);
+            return client;
+        }
+
+
+        public void Dispose()
+        {
+            if (Client != null)
+            {
+                Client.Dispose();
+                Client = null;
+            }
+
+            if (Server != null)
+            {
+                Server.Dispose();
+                Server = null;
+            }
+        }
+    }
+}
